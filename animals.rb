95e73e18a6039db95d5b4ca32090b9971393119e
Craig Rushon <craig.rushon@shopify.com>
2021-06-01 10:59:44 -0400

    Added reader and writer methods to dog class for name and age

--- animals.rb
+++ animals.rb
@@ -9,6 +9,27 @@ class Bird
 end
 
 class Dog
+
+  def name=(new_value)
+    @name = new_value
+  end
+
+  def name
+    @name
+  end
+
+  def age=(new_value)
+    @age = new_value
+  end
+
+  def age
+    @age
+  end
+
+  def report_age
+    puts "#{@name} is #{@age} years old."
+  end
+
   def talk(name)
     puts "#{name} says Bark!"
   end
